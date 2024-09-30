a9879896a1ee416a2531ad0821179d39e0605a52
夏末 <s15231212380@outlook.com>
2024-09-30 09:23:29 +0800

    Add files via upload

---
+++ week4/Lab2Task21.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Lab2Task21 {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the day number: ");
+        int day=input.nextInt();
+        switch (day) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+                break;
+            case 0:
+                System.out.println("Weekday");
+                break;
+        }
+    }
+}
